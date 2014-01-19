c1ec966987dafbe9110ad734542960f03bc30ec8
Ed Lee <eddieberklee@gmail.com>
2014-01-19 02:25:17 -0800

    working on creating events

--- db/migrate/20140118113025_create_events.rb
+++ db/migrate/20140118113025_create_events.rb
@@ -2,8 +2,8 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.string :title
-      t.time :start_time
-      t.time :end_time
+      t.datetime :start_time
+      t.datetime :end_time
 
       t.timestamps
     end
