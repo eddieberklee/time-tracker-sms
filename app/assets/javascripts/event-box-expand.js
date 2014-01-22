c9ecc6785190965bb33ee9f9bc0cd1bb16d47ba0
Ed Lee <eddieberklee@gmail.com>
2014-01-22 13:57:04 -0800

    got to a UI checkpoint

--- app/assets/javascripts/event-box-expand.js
+++ app/assets/javascripts/event-box-expand.js
@@ -4,7 +4,7 @@ $(function() {
     $this = $(this);
     if (!$this.attr('data-height')) {
       var current_height = parseInt($this.css('height'));
-      var new_height = parseInt($this.find('h2').css('height')) + 15;
+      var new_height = parseInt($this.find('h2').css('height')) + 10;
       if (new_height > current_height) {
         $this.attr('data-height',$this.css('height'));
         $this.css('height',new_height);
