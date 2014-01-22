d37bcc63f7c47d2f6402432aabc7457ca66d11a5
Ed Lee <eddieberklee@gmail.com>
2014-01-22 13:34:50 -0800

    ui in-place for handling messages that are too long

---
+++ app/assets/javascripts/event-box-expand.js
@@ -0,0 +1,25 @@
+$(function() {
+
+  $('.event-box').hover(function() {
+    $this = $(this);
+    if (!$this.attr('data-height')) {
+      var current_height = parseInt($this.css('height'));
+      var new_height = parseInt($this.find('h2').css('height')) + 15;
+      if (new_height > current_height) {
+        $this.attr('data-height',$this.css('height'));
+        $this.css('height',new_height);
+      }
+    }
+  }, function() {
+    $this = $(this);
+    if ($this.attr('data-height')) {
+      height = $this.attr('data-height');
+      console.log(height);
+      $this.css('height',height);
+      $this.removeAttr('data-height');
+    }
+  });
+
+});
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+titles = [
+  'its a good morning i feel lazy for waking up so late. i need to shower then get dressed',
+  'showering',
+  'checking up on my devices now and email and facebook',
+  'signing up for rsf',
+  'getting lunch at koja today, their rice buns are so nicely cooked',
+  'getting ready to go to cs161',
+  'getting ready to go to ugba 196 right after cs161',
+  'heading home',
+  'got home',
+  'on computer',
+  'facebook/music',
+  'studying up on technical interviews',
+  'getting my schedule for this week figured out',
+]
+
+datetimes = [
+  DateTime.new(2014,1,22,11,53,00,'-8'),
+  DateTime.new(2014,1,22,12,14,00,'-8'),
+  DateTime.new(2014,1,22,12,48,00,'-8'),
+  DateTime.new(2014,1,22,13,14,00,'-8'),
+  DateTime.new(2014,1,22,13,44,00,'-8'),
+  DateTime.new(2014,1,22,14,58,00,'-8'),
+  DateTime.new(2014,1,22,16,11,00,'-8'),
+  DateTime.new(2014,1,22,18,01,00,'-8'),
+  DateTime.new(2014,1,22,21,41,00,'-8'),
+  DateTime.new(2014,1,22,22,11,00,'-8'),
+  DateTime.new(2014,1,22,22,59,00,'-8'),
+  DateTime.new(2014,1,22,23,25,00,'-8'),
+  DateTime.new(2014,1,22,23,30,00,'-8'),
+  DateTime.new(2014,1,22,23,32,00,'-8'),
+  DateTime.new(2014,1,22,23,52,00,'-8'),
+]
+
+i = 0
+
+titles.each do |t|
+  e=Event.create(:title=>t)
+  e.created_at = datetimes[i]
+  e.start_time = datetimes[i]
+  e.end_time = datetimes[i+1]
+  e.save
+  i += 1
+end
+
+
+
+
+
